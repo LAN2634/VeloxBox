1478f4996d3fa901d7f08d02022aa99256011012
Danna <dan.prog16@gmail.comgit config --global user.name Danna>
2025-05-08 23:06:28 -0600

    Este es el de pestaña producto que ya le arregle, todavia faltan cosillas pero de mientras

--- JS/script_product.js
+++ JS/script_product.js
@@ -4,19 +4,7 @@ window.onload = function () {
 };
  // Activar menú hamburguesa
  //TODO ESTE ES PARA QUE SE DESPLEIGUE EL MENU HAMBURGUESA
- const hamburgerBtn = document.getElementById('hamburgerBtn');
- const menuOverlay = document.getElementById('menuOverlay');
- const menuContainer = document.getElementById('menuContainer');
 
- hamburgerBtn.addEventListener('click', () => {
-     menuContainer.classList.toggle('active');
-     menuOverlay.classList.toggle('active');
- });
-
- menuOverlay.addEventListener('click', () => {
-     menuContainer.classList.remove('active');
-     menuOverlay.classList.remove('active');
- });
 
  //AQUI UA ACABO
 
@@ -28,13 +16,12 @@ document.querySelectorAll('.btn-desplegar').forEach(btn => {
     });
   });
   
+  //Esto es para cambiar el color de boton de la talla seleccionada a negro
   const botonesTalla = document.querySelectorAll('.talla');
-
   botonesTalla.forEach(boton => {
     boton.addEventListener('click', () => {
       // Quitar clase activa de todos los botones
       botonesTalla.forEach(btn => btn.classList.remove('activa'));
-      
       // Agregar clase activa al botón clickeado
       boton.classList.add('activa');
     });
