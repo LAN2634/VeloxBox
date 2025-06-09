85e919a175aee8e9ec23be850f16df78d2a43edd
Danna <dan.prog16@gmail.comgit config --global user.name Danna>
2025-06-08 21:14:59 -0600

    Pequeños cambios en el index ue no me dieron chance

--- JS/script_index.js
+++ JS/script_index.js
@@ -34,6 +34,25 @@
         updateCartCount(currentCount + 1);
     }
 
+    //FOOTER MODALIDADES
+    function openVentana(id) {
+        document.getElementById(id).style.display = 'block';
+      }
+  
+      function closeVentana(id) {
+        document.getElementById(id).style.display = 'none';
+      }
+  
+      // Cierra la ventana si se hace clic en otro lado
+      window.onclick = function(event) {
+        const ventanas = document.querySelectorAll('.ventana');
+        ventanas.forEach(ventana => {
+          if (event.target === ventana) {
+            ventana.style.display = 'none';
+          }
+        });
+      }
+
     // Swiper: almacenar instancia en una variable
     let swiper;
 
