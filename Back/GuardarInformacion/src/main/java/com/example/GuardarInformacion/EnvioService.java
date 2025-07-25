c6a151f30ca4b7525f29a0bd3ebd8bda8b52bcae
LAN2634 <alansandoval1540@gmail.com>
2025-07-25 17:25:34 -0600

    actualizar rutas

--- Back/GuardarInformacion/src/main/java/com/example/GuardarInformacion/EnvioService.java
+++
@@ -1,8 +0,0 @@
-package com.example.GuardarInformacion;
-
-import java.util.Optional;
-
-public interface EnvioService {
-    Optional<Envio> getByUsuarioId(Long usuarioId);
-    Envio save(Envio envio);
-}
